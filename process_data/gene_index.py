a3b633180ba3e4bbf184dd1b4bf986307b56092f
Kuang Lu <lukuang1989@gmail.com>
2018-06-06 16:00:06 -0400

    generate indri index parameter file

---
+++ process_data/gene_index.py
@@ -0,0 +1,33 @@
+"""
+generate index
+"""
+
+import os
+import json
+import sys
+import re
+import argparse
+import codecs
+# import myUtility
+from myUtility.misc import gene_indri_index_para_file
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("corpus_dir")
+
+    parser.add_argument("index_dir")
+    parser.add_argument("--indri_para_path","-if",default="./index.para")
+    parser.add_argument("--use_stopwords","-s",action="store_true")
+    args=parser.parse_args()
+
+    field_data = []
+    field_data.append({"name":"published_date","type":"numeric"})
+    field_data.append({"name":"tt","type":"text"})
+    field_data.append({"name":"body","type":"text"})
+    gene_indri_index_para_file(args.corpus_dir,args.indri_para_path,
+                               args.index_dir,field_data=field_data,use_stopper=args.use_stopwords)
+
+if __name__=="__main__":
+    main()
+
