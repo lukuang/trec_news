452ec8b27ac0fef900076bb03d32fd9582e684bb
Kuang Lu <lukuang1989@gmail.com>
2018-08-13 15:20:11 -0400

    add option of importing annotated documents

--- process_data/import_query_doc.py
+++ process_data/import_query_doc.py
@@ -16,7 +16,13 @@ from data import ArticleGenerator,Article
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("--collection_dump","-cd",default="/infolab/node4/lukuang/trec_news/data/washington_post/collection_json_dump/v2/dump")
+    parser.add_argument("--collection_dump_root","-cdr",default="/infolab/node4/lukuang/trec_news/data/washington_post/collection_json_dump/")
+    parser.add_argument("--document_type","-dt",choices=range(2),default=0,type=int,
+        help="""
+            Choose the index type:
+                0: normal
+                1: annotated
+        """)
     args=parser.parse_args()
 
     query_dbs = [
@@ -29,8 +35,18 @@ def main():
     doc_db = redis.Redis(host=RedisDB.host,
                           port=RedisDB.port,
                           db=RedisDB.doc_db)
+    if args.document_type == 0:
+        collection_dump = os.path.join(args.collection_dump_root,"v2","dump")
+        doc_db = redis.Redis(host=RedisDB.host,
+                          port=RedisDB.port,
+                          db=RedisDB.doc_db)
+    else:
+        collection_dump = os.path.join(args.collection_dump_root,"annotated","dump")
+        doc_db = redis.Redis(host=RedisDB.host,
+                              port=RedisDB.port,
+                              db=RedisDB.annotated_doc_db)
+    collection = json.load(open(collection_dump))
 
-    collection = json.load(open(args.collection_dump))
     print "collection loaded"
     for qdb in query_dbs:
 
