a511ac7c6ded6af63b1e00169581f26e44b6062e
Kuang Lu <lukuang1989@gmail.com>
2018-08-13 09:38:51 -0400

    find top document for all queries

--- some_experiments/find_top.py
+++ some_experiments/find_top.py
@@ -1,5 +1,5 @@
 """
-find first two result for each paragraphs. 
+find top results for each paragraphs. 
 We need first two since the first one might 
 be the original article
 """
@@ -13,32 +13,37 @@ import codecs
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("src_file")
+    parser.add_argument("src_dir")
+    parser.add_argument("dest_dir")
+    parser.add_argument("--number_of_top","-nt",type=int,default=2)
     args=parser.parse_args()
 
-    dest_file = args.src_file+"_top_two"
+    for file_name in os.walk(args.src_dir).next()[2]:
+        src_file = os.path.join(args.src_dir,file_name)
+        dest_file = os.path.join(args.dest_dir,file_name)
 
-    result_dict = {}
-    output_lines = []
 
-    with open(args.src_file) as f:
-        for line in f:
-            parts = line.strip().split()
-            qid = parts[0]
-            score = float(parts[4])
-            if qid not in result_dict:
-                result_dict[qid] = set()
-            if len(result_dict[qid]) >=2:
-                continue
-            elif score in result_dict[qid]:
-                continue
-            else:
-                result_dict[qid].add(score)
-                output_lines.append(line)
+        result_dict = {}
+        output_lines = []
 
-    with open(dest_file,"w") as of:
-        for line in output_lines:
-            of.write(line)
+        with open(src_file) as f:
+            for line in f:
+                parts = line.strip().split()
+                qid = parts[0]
+                score = float(parts[4])
+                if qid not in result_dict:
+                    result_dict[qid] = set()
+                if len(result_dict[qid]) >=args.number_of_top:
+                    continue
+                elif score in result_dict[qid]:
+                    continue
+                else:
+                    result_dict[qid].add(score)
+                    output_lines.append(line)
+
+        with open(dest_file,"w") as of:
+            for line in output_lines:
+                of.write(line)
 
 if __name__=="__main__":
     main()
