0b2dbf9e3d45a210345d9983b5f30de3ca69e9e3
Kuang Lu <lukuang1989@gmail.com>
2019-04-02 15:30:00 -0400

    add support for multiple result directories

--- post_analysis/optimal_ndcg_pre_merge.py
+++ post_analysis/optimal_ndcg_pre_merge.py
@@ -10,13 +10,13 @@ import argparse
 import codecs
 import math
 
-def get_idcgs(qrels):
+def get_idcgs(qrels, ndcg_cutoff):
     idcgs = {}
     for qid in qrels:
         rels = qrels[qid].values()
         rels.sort(reverse=True)
         idcgs[qid] = .0
-        for i in xrange(5):
+        for i in xrange(min(ndcg_cutoff, len(rels))):
             idcgs[qid] += rels[i]* 1.0 / math.log(i+2, 2) 
     return idcgs
 
@@ -34,13 +34,27 @@ def read_qrel(qrel_file):
             qrels[qid][docid] = rel
     return qrels
 
-def get_results(result_dir, merge_cutoff):
+def get_results(result_dirs, merge_cutoff):
     results = {}
-    old_pid = None
-    line_num = 0
-    for qid in os.listdir(result_dir):
-        results[qid] = []
-        q_result_file = os.path.join(result_dir,qid)
+
+    for single_result_dir in result_dirs:
+        if 'doc_lm_analysis' in single_result_dir:
+            get_results_from_single_run(single_result_dir, 20, results)
+        else:
+            get_results_from_single_run(single_result_dir, merge_cutoff, results)
+    
+    for qid in results:
+        results[qid] = list(set(results[qid]))
+    return results
+
+def get_results_from_single_run(single_result_dir, merge_cutoff, results):
+    
+    for qid in os.listdir(single_result_dir):
+        old_pid = None
+        line_num = 0
+        if qid not in results:
+            results[qid] = []
+        q_result_file = os.path.join(single_result_dir,qid)
         with open(q_result_file) as f:
             for line in f:
                 cols = line.split()
@@ -56,12 +70,10 @@ def get_results(result_dir, merge_cutoff):
                     else:
                         line_num += 1
                         results[qid].append(docid)
-        results[qid] = list(set(results[qid]))
-
-    return results
 
 
-def get_optimal_dcgs(results, qrels):
+
+def get_optimal_dcgs(results, qrels, ndcg_cutoff):
     optimal_dcgs = {}
     for qid in results:
         q_rels = []
@@ -74,7 +86,8 @@ def get_optimal_dcgs(results, qrels):
                 q_rels.append(rel)
         q_rels.sort(reverse=True)
         optimal_dcgs[qid] = .0
-        for i in xrange(5):
+        for i in xrange(min(ndcg_cutoff, len(q_rels))):
+
             optimal_dcgs[qid] += q_rels[i] * 1.0 / math.log(i + 2, 2)
 
     return optimal_dcgs
@@ -83,18 +96,19 @@ def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--qrel_file', '-qf', default='/infolab/node4/lukuang/trec_news/data/eval/bqrels')
     parser.add_argument("--merge_cutoff","-mt",type=int,default=5)
-    parser.add_argument("result_dir")
+    parser.add_argument("--ndcg_cutoff","-nt",type=int,default=5)
+    parser.add_argument("result_dirs" ,nargs='+')
     args=parser.parse_args()
 
 
     qrels = read_qrel(args.qrel_file)
 
-    idcgs = get_idcgs(qrels)
+    idcgs = get_idcgs(qrels, args.ndcg_cutoff)
 
     # print idcgs
-    results = get_results(args.result_dir, args.merge_cutoff)
+    results = get_results(args.result_dirs, args.merge_cutoff)
 
-    optimal_dcgs = get_optimal_dcgs(results, qrels)
+    optimal_dcgs = get_optimal_dcgs(results, qrels, args.ndcg_cutoff)
 
     print 'optimal ndcg:'
     all_odcg = []
